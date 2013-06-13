6cfcde6ae33d993f8591ca7fddc951c87915ef36
DjDCH <coding@djdch.com>
2013-06-13 00:15:49 -0400

    Added the listener registration system.

--- src/main/java/com/djdch/simpleeventsystem/event/EventHandler.java
+++ src/main/java/com/djdch/simpleeventsystem/event/EventHandler.java
@@ -3,8 +3,21 @@ package com.djdch.simpleeventsystem.event;
 import com.djdch.simpleeventsystem.listener.ConsoleOutputListener;
 import com.djdch.simpleeventsystem.listener.ExceptionListener;
 import com.djdch.simpleeventsystem.listener.Listener;
+import com.djdch.simpleeventsystem.util.Validate;
 
 public class EventHandler {
+    public static synchronized void registerListener(Listener listener) {
+        Validate.notNull(listener, "Listener cannot be null");
+
+        if (listener instanceof ConsoleOutputListener) {
+            ConsoleOutputEvent.getHandlerList().register(listener);
+        } else if (listener instanceof ExceptionListener) {
+            ExceptionEvent.getHandlerList().register(listener);
+        } else {
+            throw new IllegalArgumentException("Unknown listener");
+        }
+    }
+
     public static synchronized void dispatch(Event event) {
         HandlerList handlers = event.getHandlers();
         Listener[] listeners = handlers.getListeners();
@@ -20,13 +33,16 @@ public class EventHandler {
 
     private static void callEvent(Listener listener, Event event) {
         switch (event.getType()) {
+            case CONSOLE_OUTPUT:
+                ((ConsoleOutputListener) listener).onConsoleOutput((ConsoleOutputEvent) event);
+                break;
+
             case EXCEPTION:
                 ((ExceptionListener) listener).onException((ExceptionEvent) event);
                 break;
 
-            case CONSOLE_OUTPUT:
-                ((ConsoleOutputListener) listener).onConsoleOutput((ConsoleOutputEvent) event);
-                break;
+            default:
+                throw new IllegalArgumentException("Unknown event type");
         }
     }
 }
--- src/main/java/com/djdch/simpleeventsystem/event/HandlerList.java
+++ src/main/java/com/djdch/simpleeventsystem/event/HandlerList.java
@@ -4,14 +4,45 @@ import java.util.ArrayList;
 
 import com.djdch.simpleeventsystem.listener.Listener;
 
+// Inspired by https://github.com/Bukkit/Bukkit/blob/master/src/main/java/org/bukkit/event/HandlerList.java
 public class HandlerList {
+    private volatile Listener[] cachedListener = null;
     private final ArrayList<Listener> listeners;
 
     public HandlerList() {
         listeners = new ArrayList<Listener>();
     }
 
+    public synchronized void register(Listener listener) {
+        if (listeners.contains(listener)) {
+            throw new IllegalStateException("This listener is already registered");
+        }
+
+        listeners.add(listener);
+        cachedListener = null;
+    }
+
+    public synchronized void unregister(Listener listener) {
+        if (listeners.remove(listener)) {
+            cachedListener = null;
+        }
+    }
+
+    private synchronized void bake() {
+        if (cachedListener != null) {
+            return; // Don't re-bake when still valid
+        }
+
+        cachedListener = listeners.toArray(new Listener[listeners.size()]);
+    }
+
     public Listener[] getListeners() {
-        return listeners.toArray(new Listener[listeners.size()]);
+        Listener[] listeners;
+
+        while ((listeners = this.cachedListener) == null) {
+            bake(); // This prevents fringe cases of returning null
+        }
+
+        return listeners;
     }
 }
